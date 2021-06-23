76ffd13148d005acf56dccaf0c71de4330973538
Hugo Costa <costarafaelhugo@gmail.com>
2021-06-23 07:47:58 -0300

    Mockei os Placeholders e fiz  Ajustes no Carrossel

--- app/src/main/java/com/hugorafaelcosta/netflixjavaversion/MainActivity.java
+++ app/src/main/java/com/hugorafaelcosta/netflixjavaversion/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
             List<Movie> movies = new ArrayList<>();
             for (int i = 0; i < 30; i++) {
                 Movie movie = new Movie();
-                movie.setCoverUrl(R.drawable.movie);
+//                movie.setCoverUrl(R.drawable.movie);
                 movies.add(movie);
             }
             category.setMovies(movies);
@@ -108,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(@NonNull MovieHolder holder, int position) {
          Movie movie = movies.get(position);
-            holder.imageViewCover.setImageResource(movie.getCoverUrl());
+//            holder.imageViewCover.setImageResource(movie.getCoverUrl());
         }
 
         @Override
